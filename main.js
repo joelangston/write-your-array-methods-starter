b36adeb8149aa1a598201493a72a18016b4313f9
Joe Langston <joelangston81@gmail.com>
2020-06-30 12:15:34 -0500

    new stuff

--- main.js
+++ main.js
@@ -12,22 +12,61 @@ function newForEach(array, callback){
     }
 }
 function newSome (array, callback){
-
+    for (let index = 0; index < array.length; index += 1){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            return true
+        }
+    }
+    return false
 
 }
 function newEvery (array, callback){
+    for (let index = 0; index < array.length; index += 1){
+        let currentItem = array[index]
+        if (callback(currentItem) === false){
+            return false
+        }
+    }
+    return true
 
 }
 function newFind (array, callback){
+    for (let index = 0; index < array.length; index += 1){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            return currentItem
+        }
+    }
+    return undefined
 
 }
 
 function newFindIndex (array, callback){
-
+    for (let index = 0; index < array.length; index += 1){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            return index
+        }
+    }
+    return -1
 }
-function newMap (array, callback){
+function newMap (array, callback){ let newArray = []
+    for (let index = 0; index < array.length; index += 1){
+        let currentItem = array[index]
+        newArray.push(callback(currentItem))
+    }
+    return newArray
 
 }
 function newFilter (array, callback) {
+    let newArray = []
+    for (let index = 0; index < array.length; index += 1){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            newArray.push(currentItem)
+        }
+    }
+    return newArray
 
 }
